6fa4ad71961d6ef0cc2bbc58e450e7134644dc74
Justin Peters <JMarvel@gmail.com>
2022-07-13 10:33:17 -0700

    initial commit
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>
    Co-authored-by: Zhibin Liang <ifthereisoneday@gmail.com>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+  }
+}
